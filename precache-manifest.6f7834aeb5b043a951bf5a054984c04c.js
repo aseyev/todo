bb75e1c80b03366fa3414a9eef2d130ba9f80547
aseyev <aseyev.1982@gmail.com>
2020-01-30 18:14:40 +0700

    Updates

---
+++ precache-manifest.6f7834aeb5b043a951bf5a054984c04c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027dd0ac7ea62d52b2a2dbd5e0a0c4fe",
+    "url": "/github.io/todospa/index.html"
+  },
+  {
+    "revision": "3ce6128bda8603ad1820",
+    "url": "/github.io/todospa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "de12f5f852835db69e72",
+    "url": "/github.io/todospa/static/js/2.61856076.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/todospa/static/js/2.61856076.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ce6128bda8603ad1820",
+    "url": "/github.io/todospa/static/js/main.65296fab.chunk.js"
+  },
+  {
+    "revision": "5933a99244e6c35729f6",
+    "url": "/github.io/todospa/static/js/runtime-main.51578006.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/todospa/static/media/logo.5d5d9eef.svg"
+  }
+]);
