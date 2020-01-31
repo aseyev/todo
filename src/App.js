f9df249f7c328b358d376e8fb824afc18f0688f0
aseyev <aseyev.1982@gmail.com>
2020-02-01 00:17:12 +0700

    first step

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Todos from './todos';
+import AddTodo from './addFormTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        // status: 0 - failed, 1 - in work, 2 - complete ???
+        todos: [
+            {id: 1, 
+                content: "be like all, do the ToDo", 
+                dateOfFinish: "00:00 07.02.2020", 
+                dateOfInput: "20:51 30.01.2020",
+                status: 1},
+            {id: 2, 
+                content: "Make it with Redux", 
+                dateOfFinish: "00:00 14.02.2020", 
+                dateOfInput: "20:51 30.01.2020",
+                status: 1},
+            {id: 3, 
+                content: "Use Material UI", 
+                dateOfFinish: "00:00 07.02.2020", 
+                dateOfInput: "20:51 30.01.2020",
+                status: 1}
+        ]
+    }
+    deleteTodo = (id) => {
+        const newTodos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        })
+        this.setState({todos: newTodos})
+    }
+    addTodo = (todo) => {
+        let lastId = this.state.todos.length - 1;
+        todo.id = (this.state.todos[lastId].id + 1)
+        let newTodos = [...this.state.todos, todo]
+        this.setState({todos: newTodos})
+    }
+    render() {
+        return (
+            <div className="todo_spa container">
+                <h1 className="center blue-text brand-logo">Todo's list</h1>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+                <AddTodo addTodo={this.addTodo} />
+            </div>
+        )
+    }
 }
+
 
 export default App;
